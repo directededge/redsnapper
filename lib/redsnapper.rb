141ee235219886c9cfc1376eed13280d76e958a2
Scott Wheeler <scott@directededge.com>
2014-03-07 21:30:39 +0100

    Make sure output isn't mangled

--- lib/redsnapper.rb
+++ lib/redsnapper.rb
@@ -1,4 +1,5 @@
 require 'thread/pool'
+require 'open3'
 
 class RedSnapper
   TARSNAP = 'tarsnap'
@@ -23,11 +24,15 @@ class RedSnapper
 
   def run
     pool = Thread.pool(THREAD_POOL_SIZE)
+    mutex = Mutex.new
 
     file_groups.each do |chunk|
       pool.process do
-        unless system(TARSNAP, '-xvf', @archive, *@options[:tarsnap_options], *chunk)
-          # mutex.syncronize { warn "Error extracting #{file}" }
+        command = [ TARSNAP, '-xvf', @archive, *@options[:tarsnap_options], *chunk ]
+        Open3.popen3(*command) do |_, _, err|
+          while line = err.gets
+            mutex.synchronize { warn line }
+          end
         end
       end
     end
