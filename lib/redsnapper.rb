57431d0d6f076e6e2a1760780d62c2bf1876c3e3
Scott Wheeler <scott@directededge.com>
2014-04-09 00:17:37 +0200

    Pedantry

--- lib/redsnapper.rb
+++ lib/redsnapper.rb
@@ -19,7 +19,7 @@ class RedSnapper
       out.gets(nil).split.reject { |f| f.end_with?('/') }
     end
 
-    files.each_slice([ (files.size.to_f / THREAD_POOL_SIZE).ceil, MAX_FILES_PER_JOB].min).to_a
+    files.each_slice([ (files.size.to_f / THREAD_POOL_SIZE).ceil, MAX_FILES_PER_JOB ].min).to_a
   end
 
   def run
