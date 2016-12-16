2489f7b2c7666e0eed28fbdd0fd1edab397ef852
Scott Wheeler <scott@directededge.com>
2016-12-16 13:59:50 +0100

    Also restore empty dirs
    This goes to a little extra trouble to make sure that only the
    outer most empty dir is passed to tarsnap.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "http://rubygems.org"
 
-gem "thread", "~> 0", :require => "thread/pool"
+gem "thread", :require => "thread/pool"
+gem "fast_trie", :require => "trie"
 
 group :development do
   gem "rdoc", "~> 3.12"
--- lib/redsnapper.rb
+++ lib/redsnapper.rb
@@ -1,5 +1,6 @@
 require 'thread/pool'
 require 'open3'
+require 'set'
 
 class RedSnapper
   TARSNAP = 'tarsnap'
@@ -15,10 +16,30 @@ class RedSnapper
     command = [ TARSNAP, '-tf', @archive, *@options[:tarsnap_options] ]
     command.push(@options[:directory]) if @options[:directory]
 
-    files = Open3.popen3(*command) do |_, out, _|
-      out.gets(nil).split("\n").reject { |f| f.end_with?('/') }
+    files = []
+    dirs = Set.new
+
+    Open3.popen3(*command) do |_, out, _|
+      out.gets(nil).split("\n").each do |entry|
+        if entry.end_with?('/')
+          dirs.add(entry)
+        else
+          files.push(entry)
+        end
+      end
     end
 
+    files.each { |f| dirs.delete(File.dirname(f) + '/') }
+    empty_dirs = dirs.clone
+
+    dirs.each do |dir|
+      components = dir.split('/')
+      components.each_with_index do |component, i|
+        empty_dirs.delete(components[0, i].join('/') + '/')
+      end
+    end
+
+    files.push(*empty_dirs)
     files.each_slice([ (files.size.to_f / THREAD_POOL_SIZE).ceil, MAX_FILES_PER_JOB ].min).to_a
   end
 
